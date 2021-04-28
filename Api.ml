850a9eca9b9f2ace4bf2d1b5cf95ac82fc67964d
Enieber Cunha <eniebercunha@gmail.com>
2021-04-28 20:45:36 -0300

    feat: creat api with post respose

---
+++ Api.ml
@@ -0,0 +1,39 @@
+let successful = ref 0
+let failed = ref 0
+
+let count_requests inner_handler request =
+  try%lwt
+    let%lwt response = inner_handler request in
+    successful := !successful + 1;
+    Lwt.return response
+
+  with exn ->
+    failed := !failed + 1;
+    raise exn
+
+
+let () =
+  Dream.run
+  @@ Dream.logger
+  @@ count_requests
+  @@ Dream.router [
+
+    Dream.get "/"
+      (fun _ ->
+        Dream.html "Good morning, world!");
+
+    Dream.get "/echo/:word"
+      (fun request ->
+        Dream.html (Dream.param "word" request));
+
+    Dream.get "/count"
+      (fun _ ->
+        Dream.html (Printf.sprintf "Count 
+        %3i success(s)!
+        %3i failed(s)!" !successful !failed));
+
+    Dream.post "/"
+      (fun _ -> Crate.post);
+
+  ]
+  @@ Dream.not_found
---
+++ create.ml
@@ -0,0 +1,23 @@
+let to_json request =
+  match Dream.header "Content-Type" request with
+  | Some "application/json" ->
+      let%lwt body = Dream.body request in
+      begin match Yojson.Basic.from_string body with
+      | exception _ -> Lwt.return None
+      | json -> Lwt.return (Some json)
+      end
+      | _ -> Lwt.return None
+      
+let post () =
+  (fun request ->
+    match%lwt to_json request with
+    | None -> Dream.empty `Bad_Request
+    | Some json ->
+        let maybe_message =
+          Yojson.Basic.Util.(member "message" json |> to_string_option) in
+        match maybe_message with
+        | None -> Dream.empty `Bad_Request
+        | Some message ->            
+          `String message
+          |> Yojson.Basic.to_string
+          |> Dream.json);
